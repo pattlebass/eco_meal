546dd8d528fed0c72cf293e4416a3d2ce6ed4a82
Fabian S. <49322676+pattlebass@users.noreply.github.com>
2026-07-16 18:03:26 +0300

    Add Confirm password field
    Sper sa nu fi stricat nimic

--- src/Form/BusinessRegistrationFormType.php
+++ src/Form/BusinessRegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,12 +19,22 @@ class BusinessRegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
                 'required' => $options['passwordRequired'],
-                'attr' => ['autocomplete' => 'new-password'],
+                'invalid_message' => 'The passwords do not match.',
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'label' => 'Password',
+                ],
+                'second_options' => [
+                    'label' => 'Confirm password',
+                ],
                 'constraints' => [
                     new NotBlank(
                         message: 'Please enter a password',
@@ -31,7 +42,6 @@ class BusinessRegistrationFormType extends AbstractType
                     new Length(
                         min: 6,
                         minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
                 ],
